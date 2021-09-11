ecd02acb70955ec91846185016eca465d5676faa
masaya821 <m.tomizawa821@gmail.com>
2021-09-12 05:04:39 +0900

    要素の縦幅調整

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <script src="<?php echo get_template_directory_uri(); ?>/js/particles.min.js?date=202108028"></script>
     <link rel="stylesheet" href="<?php  echo get_template_directory_uri(); ?>/style.css" type="text/css">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/reset.css" type="text/css">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/icomoon/style.css" type="text/css">
